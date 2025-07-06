eb585b8e9631074e0884d5fb64f6dcc4e42137f5
Akhil <10akhil.t@gmail.com>
2025-07-06 22:01:11 +0530

    Changed intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
 "use client";
 import { useState } from "react";
 import Hero from "@/components/Hero";
-import Intro from "@/components/Intro";
+//import Intro from "@/components/Intro";
+
+// export default function Home() {
+//   const [showIntro, setShowIntro] = useState(true);
+
+//   return showIntro ? (
+//     <Intro onComplete={() => setShowIntro(false)} />
+//   ) : (
+//     <Hero />
+//   );
+// }
 
 export default function Home() {
-  const [showIntro, setShowIntro] = useState(true);
-
-  return showIntro ? (
-    <Intro onComplete={() => setShowIntro(false)} />
-  ) : (
-    <Hero />
-  );
+  return (
+    <>
+    <div className=" w-full h-full bg-zinc-950 flex justify-center items-center overflow-hidden">
+        <Hero/>
+    </div>
+    </>
+  )
 }
-

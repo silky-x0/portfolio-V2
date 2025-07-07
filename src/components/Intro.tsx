13671813cbdc6355f31da2976e2ef138531dcb47
Akhil <10akhil.t@gmail.com>
2025-07-07 21:50:04 +0530

    Added Intro Animation

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,25 +1,73 @@
-import React from 'react';
+"use client";
 
-const Intro: React.FC = () => {
-  return (
-      <>
-      <div className='svg flex justify-center items-center fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden'>
-          <svg viewBox="0 0 400 200" xmlns="http://www.w3.org/2000/svg">
-  <defs>
-    <mask id="hiMask">
-      <rect width="400" height="200" fill="white"/>
-  
-      <text x="200" y="120" font-family="system-ui, -apple-system, sans-serif" font-size="80" font-weight="300" text-anchor="middle" fill="black">Hi</text>
-    </mask>
-  </defs>
-  
-  <rect width="400" height="200" fill="#000000"/>
-  
-  <rect width="400" height="200" fill="black" mask="url(#hiMask)"/>
-</svg>
-      </div>
-      </>
-  );
+import React from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+interface IntroProps {
+	onComplete: () => void;
+}
+
+const Intro: React.FC<IntroProps> = ({ onComplete }) => {
+	useGSAP(() => {
+		const tl = gsap.timeline({
+			onComplete,
+		});
+
+		tl.to(".hiMask-group", {
+			rotate: 10,
+			duration: 2,
+			ease: "Power4.easeInOut",
+			transformOrigin: "50% 50%",
+		}).to(".hiMask-group", {
+			scale: 10,
+			duration: 2,
+			delay: -1.8,
+			ease: "Expo.easeInOut",
+			transformOrigin: "50% 50%",
+			opacity: 0,
+		});
+	}, [onComplete]);
+
+	return (
+		<div className='svg flex justify-center items-center fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden'>
+			<svg
+				viewBox='0 0 800 600'
+				preserveAspectRatio='xMidYMid slice'
+			>
+				<defs>
+					<mask id='hiMask'>
+						<rect
+							width='100%'
+							height='100%'
+							fill='black'
+						/>
+						<g className='hiMask-group'>
+							<text
+								x='50%'
+								y='50%'
+								fontSize='250'
+								textAnchor='middle'
+								fill='white'
+								dominantBaseline='middle'
+								fontFamily='Arial Black'
+							>
+								Hi
+							</text>
+						</g>
+					</mask>
+				</defs>
+
+				<image
+					href='https://images.unsplash.com/photo-1506744038136-46273834b3fb'
+					width='100%'
+					height='100%'
+					preserveAspectRatio='xMidYMid slice'
+					mask='url(#hiMask)'
+				/>
+			</svg>
+		</div>
+	);
 };
 
 export default Intro;

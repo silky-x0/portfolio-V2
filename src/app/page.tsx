74f66951b6216add1fd11a79c288af5428fdd381
Akhil <10akhil.t@gmail.com>
2025-07-07 21:49:12 +0530

    Added logic based rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,18 @@
 "use client";
 import { useState } from "react";
 import Hero from "@/components/Hero";
-//import Intro from "@/components/Intro";
-
-// export default function Home() {
-//   const [showIntro, setShowIntro] = useState(true);
-
-//   return showIntro ? (
-//     <Intro onComplete={() => setShowIntro(false)} />
-//   ) : (
-//     <Hero />
-//   );
-// }
+import Intro from "@/components/Intro";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
-  return (
+  const [showIntro, setShowIntro] = useState(true);
+
+  return showIntro ? (
+    <Intro onComplete={() => setShowIntro(false)} />
+  ) : (
     <>
-    <div className=" w-full h-full bg-zinc-950 flex justify-center items-center overflow-hidden">
-        <Hero/>
-    </div>
+      <Navbar />
+      <Hero />
     </>
-  )
+  );
 }

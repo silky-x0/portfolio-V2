6b1658c4f035f33eace999da340c8c0dd5a38a22
Akhil <10akhil.t@gmail.com>
2025-07-07 21:48:16 +0530

    removed navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,21 @@
 import type { Metadata } from "next";
 import "../styles/globals.css";
-import Navbar from "@/components/Navbar";
-
 
 export const metadata: Metadata = {
-  title: "Akhilesh",
-  description: "A Portolio App",
+	title: "Akhilesh",
+	description: "A Portolio App",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className="bg-black text-white scroll-smooth"
-      >
-      <Navbar />
-      <main className="min-h-screen">{children}</main>
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body className='bg-black text-white scroll-smooth'>
+				<main className='min-h-screen'>{children}</main>
+			</body>
+		</html>
+	);
 }

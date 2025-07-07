b06506cc52978a39b5899d4d420d49f417753983
Akhil <10akhil.t@gmail.com>
2025-07-07 23:20:42 +0530

    Added typesafety

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
 	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
 	return (
 		<html lang='en'>
 			<body className='bg-black text-white scroll-smooth'>

82e9033e252d39400c0bbb6f831bbd49009f5be5
Akhil <10akhil.t@gmail.com>
2025-07-08 12:58:51 +0530

    Added export fxn

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <div>
+      {/* Your about page content */}
+      Contact Page
+    </div>
+  );
+}
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,8 @@
+export default function Projects() {
+  return (
+    <div>
+      {/* Your about page content */}
+      Projects Page
+    </div>
+  );
+}

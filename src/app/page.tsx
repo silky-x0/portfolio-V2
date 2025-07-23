32687d44c0822fe5183c224831a6215bf4161583
Akhil <10akhil.t@gmail.com>
2025-07-23 21:44:35 +0530

    Temporary removed intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,24 @@ import Hero from "@/components/Hero";
 import Intro from "@/components/Intro";
 import ExperiencePage from "./experiance/page";
 export default function Home() {
-  const [showIntro, setShowIntro] = useState(true);
+	const [showIntro, setShowIntro] = useState(true);
 
-  return showIntro ? (
-    <Intro onComplete={() => setShowIntro(false)} />
-  ) : (
-    <>
-      {/* <Navbar /> */}
-      <Hero />
-      <ExperiencePage />
-    </>
-  );
+	//   return showIntro ? (
+	//     <Intro onComplete={() => setShowIntro(false)} />
+	//   ) : (
+	//     <>
+	//       {/* <Navbar /> */}
+	//       <Hero />
+	//       <ExperiencePage />
+	//     </>
+	//   );
+	// }
+
+	return (
+		<>
+			{/* <Navbar /> */}
+			<Hero />
+			<ExperiencePage />
+		</>
+	);
 }

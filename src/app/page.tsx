f791554aaf4d02f00fc2d7f0ac2a2de59cf8a23c
Akhil <10akhil.t@gmail.com>
2025-07-27 23:26:53 +0530

    Commented out to fix deploymnt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+// import { useState } from "react";
 import Hero from "@/components/Hero";
 // import Intro from "@/components/Intro";
 import ExperiencePage from "./experiance/page";
